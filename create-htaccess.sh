18f8daf0427229899dd8f7893801d105aae330ea
Bryan Heden <b.heden@gmail.com>
2020-10-17 23:16:15 -0500

    fix create htaccess

--- create-htaccess.sh
+++ create-htaccess.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # find all php files, get rid of the prepended "./", then get rid of everything in assets and template directories
-files=$(find . -name "*.php" | sed 's|^./||' | grep -v "^assets" | grep -v "^template")
+# then get rid of the php extension
+files=$(find . -name "*.php" | sed 's|^./||' | grep -v "^assets" | grep -v "^template" | sed 's|\.php$||')
 
 echo "RewriteEngine On" > .htaccess
 
 for file in $files; do
-    echo "RewriteRule /${file} /${file}.php [L]" >> .htaccess
+    echo "RewriteRule /${file}\$ /${file}.php [L]" >> .htaccess
 done
